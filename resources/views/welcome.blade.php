ea8549cbbfc15b5a755969a80aa56a200d751a19
damiancp <damiancp@hotmail.com>
2016-05-04 00:14:48 -0500

    Se actualizo el index
    Index responsivo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,71 +3,134 @@
 @section('title', 'Bienvenido')
 
 @section('content')
-<div id="subhead_container">
-        
-    <div class="row">
+<!-- Page Content -->
+    <div class="container">
 
-        <div class="jumbotron">
-          <div id="features-wrapper">
-					<section id="features" class="container">
-						<header>
-							<h2>Compra en USA y recibe en <strong>M&eacute;xico</strong>!</h2>
-						</header>
-						<div class="row">
-							<div class="4u 12u(mobile)">
+        <!-- Jumbotron Header -->
+        <header class="jumbotron hero-spacer">
+        	<!-- Inicio de carrusel -->
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+			    	<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			    	<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+	    			<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+	    			<li data-target="#carousel-example-generic" data-slide-to="4"></li>
+		  		</ol>
 
-								<!-- Feature -->
-									<section>
-										<a href="#" class="image featured"><img src="{{asset('images/reception.png')}}" alt="" /></a>
-										<header>
-											<h3>Recibimos tu paquete</h3>
-										</header>
-										<p>This is <strong>Strongly Typed</strong>, a free, fully responsive site template
-										by <a href="http://html5up.net">HTML5 UP</a>. Free for personal and commercial use under the
-										<a href="http://html5up.net/license">CCA 3.0 license</a>.</p>
-									</section>
+				<!-- Wrapper for slides -->
+  				<div class="carousel-inner" role="listbox">
+    				<div class="item active">
+    					<img src="{{ asset('/images/laptop.png') }}" class="img-responsive center-block img-circle" />
+      					<div class="carousel-caption">
+      						<div class="degradadoAzul"><h3 style="color:#ffffff;">COMPRA LO QUE QUIERAS ONLINE</h3></div>
+      					</div>
+    				</div>
+    				<div class="item">
+                        <img src="{{ asset('/images/warehouse.png') }}" class="img-responsive center-block img-circle" />
+      					<div class="carousel-caption">
+      						<div class="degradadoAzul"><h3 style="color:#ffffff;">MANDALO A NOSOTROS EN ESTADOS UNIDOS</h3></div>
+      					</div>
+					</div>
+					<div class="item">
+                        <img src="{{ asset('/images/email.png') }}" class="img-responsive center-block img-circle" />
+      					<div class="carousel-caption">
+      						<div class="degradadoAzul"><h3 style="color:#ffffff;">TE AVISAMOS CUANDO LLEGUE</h3></div>
+      					</div>
+					</div>
+					<div class="item">
+                        <img src="{{ asset('/images/send.jpg') }}" class="img-responsive center-block img-circle" />
+      					<div class="carousel-caption">
+      						<div class="degradadoAzul"><h3 style="color:#ffffff;">AUTORIZA EL TRASLADO DE TU PAQUETE A MEXICO</h3></div>
+      					</div>
+					</div>
+					<div class="item">
+                        <img src="{{ asset('/images/recieve.jpg') }}" class="img-responsive center-block img-circle" />
+      					<div class="carousel-caption">
+      						<div class="degradadoAzul"><h3 style="color:#ffffff;">RECIBELO EN TU CASA</h3></div>
+      					</div>
+					</div>    
+  				</div>
 
-							</div>
-							<div class="4u 12u(mobile)">
+			  	<!-- Controls -->
+			  	<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+			    	<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			    	<span class="sr-only">Previous</span>
+			  	</a>
+			  	<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			    	<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			    	<span class="sr-only">Next</span>
+			  	</a>
+			</div>
+			<!-- Fin de carrusel -->
+        </header>
 
-								<!-- Feature -->
-									<section>
-										<a href="#" class="image featured"><img src="{{asset('images/transit.png')}}" alt="" /></a>
-										<header>
-											<h3>Lo trasladamos</h3>
-										</header>
-										<p><a href="http://html5up.net">HTML5 UP</a> is a side project of <a href="http://n33.co">AJ’s</a> (= me).
-										I started it as a way to both test my <strong>skel</strong> framework and sharpen up my coding
-										and design skills a bit.</p>
-									</section>
+        <hr>
 
-							</div>
-							<div class="4u 12u(mobile)">
+        <!-- Title -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h3>Conocenos</h3>
+            </div>
+        </div>
+        <!-- /.row -->
 
-								<!-- Feature -->
-									<section>
-										<a href="#" class="image featured"><img src="{{asset('images/delivery.png')}}" alt="" /></a>
-										<header>
-											<h3>Lo recibes en M&eacute;xico</h3>
-										</header>
-										<p><strong>Skel</strong> is a lightweight framework for building responsive
-										sites and apps. All of my stuff at <a href="http://html5up.net">HTML5 UP</a> (including this
-										one) are built on this framework.</p>
-									</section>
+        <!-- Page Features -->
+        <div class="row text-center">
 
-							</div>
-						</div>
-						<ul class="actions">
-							<li><a href="#" class="button icon fa-file">M&aacute;s Detalles</a></li>
-						</ul>
-					</section>
-				</div>
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="{{ asset('/images/rack1.jpg') }}" alt="">
+                    <div class="caption">
+                        <h4>Desde USA a México</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <!--a href="#" class="btn btn-default">Buy Now!</a--><a href="#" class="btn btn-primary">Ver más</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="{{ asset('/images/rack2.jpg') }}" alt="">
+                    <div class="caption">
+                        <h4>Información en línea</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <!--a href="#" class="btn btn-primary">Buy Now!</a--><a href="#" class="btn btn-primary">Ver más</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="{{ asset('/images/rack3.jpg') }}" alt="">
+                    <div class="caption">
+                        <h4>Marcas americanas para tí</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <!--a href="#" class="btn btn-primary">Buy Now!</a--><a href="#" class="btn btn-primary">Ver más</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="{{ asset('/images/rack4.jpg') }}" alt="">
+                    <div class="caption">
+                        <h4>Envios a todo México</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <!--a href="#" class="btn btn-primary">Buy Now!</a--><a href="#" class="btn btn-primary">Ver más</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
 
         </div>
-            
-    </div>
-</div>
-
-
+        <!-- /.row -->
 @endsection
